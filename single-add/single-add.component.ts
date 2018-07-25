ab831d66ef6de99a8206b01a18ca3f7426aff904
su-raz <suraj.bh46@gmail.com>
2018-07-25 04:12:31 -0700

    new uploaded file

---
+++ single-add/single-add.component.ts
@@ -0,0 +1,31 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { HttpClient } from '@angular/common/http';
+
+@Component({
+  selector: 'app-single-add',
+  templateUrl: './single-add.component.html',
+  styleUrls: ['./single-add.component.css']
+})
+export class SingleAddComponent implements OnInit {
+payment={user:{}};
+  constructor(private http: HttpClient, private router: Router) { }
+
+  ngOnInit() {
+  	
+
+  	}
+  	savePayment(){
+
+  		this.http.post('http://192.168.1.77/payment/', this.payment).suscribe(res =>{
+  			let id =res['id '];
+  			this.router.navigate(['/single-add', id]);
+  	 }, (err) => {
+          console.log(err);
+        }
+
+      );
+
+  }
+
+}
